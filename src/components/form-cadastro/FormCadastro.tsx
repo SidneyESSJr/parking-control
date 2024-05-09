46e7365aab2d09791a8a50b41d0f73af0da094f0
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-05-09 14:06:33 -0300

    feat: adiciona elementos opetion ao select

--- src/components/form-cadastro/FormCadastro.tsx
+++ src/components/form-cadastro/FormCadastro.tsx
@@ -1,3 +1,4 @@
+import { getApt, getBloco, getVagas } from "../../util/funcoes";
 import Input from "../wrapper/input/Input";
 import Select from "../wrapper/select/Select";
 import styles from "./formCadastro.module.css";
@@ -6,9 +7,9 @@ export default function FormCadastro() {
   return (
     <form className={styles.form}>
       <Input id="nome" label="Nome do morador" />
-      <Select id="" label="Número do apartamento" options={[]} />
-      <Select id="" label="Bloco do apartamento" options={[]} />
-      <Select id="" label="Número da vaga" options={[]} />
+      <Select id="" label="Número do apartamento" options={getApt()} />
+      <Select id="" label="Bloco do apartamento" options={getBloco()} />
+      <Select id="" label="Número da vaga" options={getVagas()} />
       <Input id="nome" label="Modelo do veículo" />
       <Input id="nome" label="Placa do veículo" />
       <Input id="nome" label="Cor do veículo" />
