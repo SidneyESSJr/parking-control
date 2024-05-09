3ceb956c281c5d950a3f265414068ce0fda76002
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-05-09 14:44:09 -0300

    feat: adiciona a configuração incial da lib zod para validação

--- src/components/form-cadastro/FormCadastro.tsx
+++ src/components/form-cadastro/FormCadastro.tsx
@@ -3,6 +3,8 @@ import { getApt, getBloco, getVagas } from "../../util/funcoes";
 import Input from "../wrapper/input/Input";
 import Select from "../wrapper/select/Select";
 import styles from "./formCadastro.module.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import schema from "../../util/validations";
 
 export type FormData = {
   nome: string;
@@ -19,7 +21,9 @@ export default function FormCadastro() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
   const onSubmit = handleSubmit((data) => {
     console.log(data);
@@ -28,24 +32,29 @@ export default function FormCadastro() {
   return (
     <form className={styles.form} onSubmit={onSubmit}>
       <Input {...register("nome")} label="Nome do morador" />
+
       <Select
         {...register("apto")}
         label="Número do apartamento"
         options={getApt()}
       />
+
       <Select
         {...register("bloco")}
         label="Bloco do apartamento"
         options={getBloco()}
       />
+
       <Select
         {...register("vaga")}
         label="Número da vaga"
         options={getVagas()}
       />
+
       <Input {...register("modelo")} label="Modelo do veículo" />
       <Input {...register("placa")} label="Placa do veículo" />
       <Input {...register("cor")} label="Cor do veículo" />
+
       <button className="button" type="submit">
         Cadastrar
       </button>
