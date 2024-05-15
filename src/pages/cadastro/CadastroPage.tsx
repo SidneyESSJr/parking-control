7b9a6cedcca84f32e454717d48f94e30e9890aed
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-05-15 08:16:48 -0300

    feat: adiciona responsividade ao layout da aplicação

--- src/pages/cadastro/CadastroPage.tsx
+++ src/pages/cadastro/CadastroPage.tsx
@@ -5,8 +5,10 @@ export default function CadastroPage() {
   return (
     <main className={styles.cadastro}>
       <h1 className="title animeLeft">Cadastro de vagas</h1>
-      <FormCadastro />
-      <img src="/parking-control.webp" alt="parking control image" />
+      <div className={styles.container}>
+        <FormCadastro />
+        <img src="/parking-control.webp" alt="parking control image" />
+      </div>
     </main>
   );
 }
